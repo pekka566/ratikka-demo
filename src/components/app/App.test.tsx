be8cf504a0133a5efa0c44bd5d249dd576a556f7
pekkas <pekka.suomala@gmail.com>
2021-07-10 15:25:21 +0300

    fix directory cases

--- src/components/app/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { render, screen } from "@testing-library/react"
-import { App } from "./App"
-
-test("renders App component", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Ratikka-demo/i)
-  expect(linkElement).toBeInTheDocument()
-})
